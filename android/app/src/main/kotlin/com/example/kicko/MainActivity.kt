8e5faf1648aba8b5fb23586cc255f7560e7b5bc5
Sasha <mouss42490@gmail.com>
2022-09-10 18:24:03 +0200

    Add platforms.

---
+++ android/app/src/main/kotlin/com/example/kicko/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kicko
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
   );
 
   runApp(const MaterialApp(
-    title: 'Navigation Basics',
+    title: 'KICKO!',
     home: KickoApp(),
   ));
 }
