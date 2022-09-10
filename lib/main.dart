77d00c76d13277dde170c6ad3c57ed8df7952a8f
Sasha <mouss42490@gmail.com>
2022-09-10 18:22:13 +0200

    Better ititialize firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:kicko/professional/main.dart';
 import 'package:flutter/material.dart';
-import 'dio.dart';
+// import 'dio.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MaterialApp(
     title: 'Navigation Basics',
     home: KickoApp(),
@@ -20,38 +26,21 @@ class KickoApp extends StatefulWidget {
 
 class _KickoApp extends State<KickoApp>{
 
-  bool _initialized = false;
-
-  void initializeFlutterFire() async {
-    try {
-      // Wait for Firebase to initialize and set `_initialized` state to true
-      await Firebase.initializeApp();
-      setState(() {
-        _initialized = true;
-      });
-    } catch(e) {
-      // Set `_error` state to true if Firebase initialization fails
-      setState(() {
-        _initialized = false;
-      });
-    }
-  }
-
   @override
   void initState() {
-    initializeFlutterFire();
     super.initState();
   }
 
 
   @override
   Widget build(BuildContext context) {
+    Container container;
+
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text('KICKO'),
+          title: const Text('First Route'),
         ),
-
-
         body: Container(
             alignment: Alignment.center,
             child: Wrap(
@@ -78,9 +67,9 @@ class _KickoApp extends State<KickoApp>{
                     },
                   ),
 
-                ]))
+                ])
 
-    );
+        ));
   }
 }
 
