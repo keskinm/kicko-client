22681abb0b2906f720e49df10e132b46a4d3d8aa
Sasha <mouss42490@gmail.com>
2022-09-10 17:34:05 +0200

    Import firebase_auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,48 @@
 import 'package:kicko/professional/main.dart';
 import 'package:flutter/material.dart';
 import 'dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
 void main() {
   runApp(const MaterialApp(
     title: 'Navigation Basics',
-    home: FirstRoute(),
+    home: KickoApp(),
   ));
 }
 
-class FirstRoute extends StatelessWidget {
-  const FirstRoute({Key? key}) : super(key: key);
+class KickoApp extends StatefulWidget {
+  const KickoApp({Key? key}) : super(key: key);
+
+  @override
+  _KickoApp createState() => _KickoApp();
+}
+
+class _KickoApp extends State<KickoApp>{
+
+  bool _initialized = false;
+
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch(e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _initialized = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +73,7 @@ class FirstRoute extends StatelessWidget {
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => const FirstRoute()),
+                        MaterialPageRoute(builder: (context) => const KickoApp()),
                       );
                     },
                   ),
