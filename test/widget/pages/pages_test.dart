c66a2c891f7a395df4aa644b156664d71ac796b4
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-02-24 21:31:54 +0100

    format

--- test/widget/pages/pages_test.dart
+++ test/widget/pages/pages_test.dart
@@ -3,7 +3,6 @@ import 'package:kicko/pages/candidate/candidate_home_page.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   testWidgets('shows messages', (WidgetTester tester) async {
     final firestore = FakeFirebaseFirestore();
@@ -13,11 +12,11 @@ void main() {
       'created_at': DateTime.now(),
     });
 
-    await tester.pumpWidget(MaterialApp(home: CandidateHome(firestore: firestore)));
+    await tester
+        .pumpWidget(MaterialApp(home: CandidateHome(firestore: firestore)));
 
     await tester.pumpAndSettle();
 
     expect(find.text('Hello world!'), findsOneWidget);
   });
 }
-
