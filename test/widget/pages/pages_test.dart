50719dd2e2178530a66987bc70314fb7e2b48dde
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-02-24 20:55:13 +0100

    try fixes

--- test/widget/pages/pages_test.dart
+++ test/widget/pages/pages_test.dart
@@ -1,4 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:kicko/pages/candidate/candidate_home_page.dart';
+import 'package:kicko/firebase_options.dart';
+
 // void main() {
 //   test('test_name', () {
 //     var expected = 42;
@@ -8,7 +12,12 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:kicko/pages/candidate/candidate_home_page.dart';
 
-void main() {
+void main() async {
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   testWidgets('Mon test de widget', (WidgetTester tester) async {
     await tester.pumpWidget(CandidateHome());
 
