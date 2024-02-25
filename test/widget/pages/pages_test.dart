552e4cc174bd97b8f6fe17595d1bd3075c76b012
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-02-25 10:40:11 +0100

    ongoing proto test

--- test/widget/pages/pages_test.dart
+++ test/widget/pages/pages_test.dart
@@ -2,21 +2,42 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kicko/pages/candidate/candidate_home_page.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:kicko/services/database.dart';
+import 'package:mockito/mockito.dart';
+
 
 void main() {
   testWidgets('shows messages', (WidgetTester tester) async {
-    final firestore = FakeFirebaseFirestore();
+    final fakeFirestore = FakeFirebaseFirestore();
+    final mockDatabaseService = DatabaseMethods(firestore: fakeFirestore);
 
-    await firestore.collection('messages').add({
-      'message': 'Hello world!',
-      'created_at': DateTime.now(),
-    });
+    await mockDatabaseService.addMessage("EH", {'coucou': 'hello world'});
 
-    await tester
-        .pumpWidget(MaterialApp(home: CandidateHome(firestore: firestore)));
+    final message = await fakeFirestore.collection("chatRoom")
+        .doc("EH")
+        .collection("chats").get();
+    for (var document in message.docs) {
+      print(document.data());
+    }
 
-    await tester.pumpAndSettle();
+    // final messages = await fakeFirestore.collection('messages').get();
+    // expect(messages.docs.length, 1);
+    // expect(messages.docs.first['message'], 'Hello world!');
 
-    expect(find.text('Hello world!'), findsOneWidget);
+    // ----------------------------------------
+
+    // await fakeFirestore.collection('messages').add({
+    //   'message': 'Hello world!',
+    //   'created_at': DateTime.now(),
+    // });
+
+    // await tester.pumpWidget(
+    //   Provider<DatabaseService>(
+    //     create: (_) => mockDatabaseService,
+    //     child: MaterialApp(home: CandidateHome()),
+    //   ),
+    // );
+
   });
 }
