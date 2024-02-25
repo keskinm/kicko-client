c40f8d07796739c18fa9bbf35bcb3575d8330768
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-02-25 18:15:10 +0100

    ong

--- test/unit/database_services/dio_mockito_test.dart
+++ test/unit/database_services/dio_mockito_test.dart
@@ -1,29 +1,49 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:mockito/mockito.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'my_api_service.dart';
+import 'package:kicko/easy_tests/test_page.dart';
 
-class MockDio extends Mock implements Dio {}
+
+class MockDio extends Mock implements Dio {
+
+}
 
 void main() {
   group('MyApiService Tests', () {
     late MockDio mockDio;
-    late MyApiService myApiService;
+    // late MyApiService myApiService;
 
     setUp(() {
       mockDio = MockDio();
-      myApiService = MyApiService(dio: mockDio);
+      // myApiService = MyApiService(dio: mockDio);
     });
 
     test('Test GET request', () async {
       final responsePayload = {'key': 'value'};
-      when(mockDio.get(any as String)).thenAnswer((_) async => Response(data: responsePayload, requestOptions: RequestOptions(path: ''), statusCode: 200));
+      // Use `any` properly for non-nullable parameter
+      when(mockDio.get('fake_route'))
+          .thenAnswer((_) async => Response(
+                data: responsePayload, 
+                requestOptions: RequestOptions(path: ''),
+                statusCode: 200,
+              ));
 
-      var result = await myApiService.fetchData();
+      // var result = await myApiService.fetchData();
+      // expect(result, equals(responsePayload));
+      // dynamic results = MockDio().get('fake_route');
+      // print("results? ${results}");
 
-      expect(result, equals(responsePayload));
+      // await tester.pumpWidget(
+      //   Provider<Dio>.value(
+      //     value: mockDio,
+      //     child: MyApp(), // Replace with your actual app widget
+      //   ),
+      // );
+
+
     });
   });
 }
