abd7656f2952dacd59543c9f24256b58f682c59f
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-03-24 07:08:59 +0100

    Fix

---
+++ lib/user/domain/my_account_logic.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:kicko/professional/domain/models.dart';
+import 'package:kicko/professional/ui/professional_home_page.dart';
+import 'package:kicko/shared/validator.dart';
+import 'package:kicko/services/app_state.dart';
+import 'package:kicko/services/firebase.dart';
+import 'package:kicko/shared/common.dart';
+import 'package:provider/provider.dart';
+
+class AccountLogic {
+  Future<String> getProfileImage(
+      Future<Map<String, dynamic>> futureBusinessJson,
+      String imagesBucket,
+      BuildContext context) async {
+    String bucket;
+
+    Map<String, dynamic> businessJson = await futureBusinessJson;
+
+    String? profileImageId = businessJson['image_id'];
+
+    if (profileImageId == null) {
+      bucket = 'default';
+      profileImageId = 'ca_default_profile.jpg';
+    } else {
+      bucket = imagesBucket;
+    }
+
+    return Provider.of<FireBaseServiceInterface>(context, listen: false)
+        .downloadFile(bucket, profileImageId);
+  }
+}
