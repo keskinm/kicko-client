3a74af8e51c898a8c1b17906cabc61f426583b2f
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-03-24 12:21:50 +0100

    Fix

--- test/widget/ui/candidate_tree_test.dart
+++ test/widget/ui/candidate_tree_test.dart
@@ -11,6 +11,14 @@ import 'package:dio/dio.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
 import 'package:kicko/get_it_service_locator.dart';
 import 'package:kicko/services/app_state.dart';
+import 'package:kicko/services/network_image.dart';
+
+class MockImageNetworkService implements ImageNetworkServiceInterface {
+  @override
+  ImageProvider getImageProvider(String imageUrl) {
+    return AssetImage('assets/images/PNG_transparency_demonstration_1.png');
+  }
+}
 
 void main() {
   setupFirebaseAuthMocks();
@@ -108,13 +116,14 @@ void main() {
       });
 
     await tester.pumpWidget(
-      Provider<FireBaseServiceInterface>(
-        create: (_) => FireBaseService(
-            firestore: fakeFirestore, firebaseStorage: fakeStorage),
-        child: MaterialApp(
-          home: CandidateHome(),
+      MultiProvider(providers: [
+        Provider<FireBaseServiceInterface>(
+            create: (_) => FireBaseService(
+                firestore: fakeFirestore, firebaseStorage: fakeStorage)),
+        Provider<ImageNetworkServiceInterface>(
+          create: (_) => MockImageNetworkService(),
         ),
-      ),
+      ], child: MaterialApp(home: CandidateHome())),
     );
 
     // Check for loading indicators before data is fetched with pumpAndSettle
